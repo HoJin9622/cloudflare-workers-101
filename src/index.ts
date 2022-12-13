548b387803d43818106cefa2bc117b174119e79a
HoJin9622 <kiss0104040@gmail.com>
2022-12-14 01:43:00 +0900

    #23.2 Routing

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {}
 
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-}
+// @ts-ignore
+import home from './home.html'
 
 export default {
   async fetch(
@@ -25,6 +9,14 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response('it works!!')
+    const url = new URL(request.url)
+    if (url.pathname === '/') {
+      return new Response(home, {
+        headers: {
+          'Content-Type': 'text/html;charset=utf-8',
+        },
+      })
+    }
+    return new Response(null, { status: 404 })
   },
 }
