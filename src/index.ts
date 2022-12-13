9ada1b298a8c7108a7fbe17552713752d0c5e445
HoJin9622 <kiss0104040@gmail.com>
2022-12-14 02:39:10 +0900

    #23.5 Conclusions

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export interface Env {
 
 // @ts-ignore
 import home from './home.html'
+import { makeBadge } from './utils'
 
 function handleHome() {
   return new Response(home, {
@@ -34,9 +35,9 @@ async function handleVisit(searchParams: URLSearchParams, env: Env) {
     value = parseInt(kvPage) + 1
     await env.DB.put(page, value + '')
   }
-  return new Response(JSON.stringify({ visits: value }), {
+  return new Response(makeBadge(value), {
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'image/svg+xml;charset=utf-8',
     },
   })
 }
